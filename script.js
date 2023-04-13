fb063694d25f14c82f250d539ac896ef5cadb581
cptndunsel <110208740+cptndunsel@users.noreply.github.com>
2023-04-12 23:14:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.querySelector('#todoform').addEventListener("submit", function (event) {
     event.preventDefault();
     var newEl = document.createElement("li");
+
+    if (textfield.value === '') {
+        return; //prevents submission of empty input field
+    }
+
     newEl.textContent = textfield.value;
-    textfield.value = '';
+    textfield.value = ''; //resets the input to blank
 
     document.body.appendChild(newEl);
-
+    
 })
